f55704ffa45d6438888bf2b281e4f8f5e979ab07
Takahiro Sugiura <takahiro.sugiura.0227@gmail.com>
2017-05-26 14:36:01 +0900

    Add Token class

--- src/animal/Token.java
+++ src/animal/Token.java
@@ -1,7 +1,17 @@
 package animal;
 
-/**
- * Created by Takahiro on 2017/05/26.
- */
-public class Token {
+public abstract class Token {
+    public static final Token EOF = new Token(-1){};
+    public static final String EOL = "\\n";
+    private int lineNumber;
+
+    protected Token(int line){
+        lineNumber = line;
+    }
+    public int getLineNumber() { return lineNumber;}
+    public boolean isIdentifier() { return false;}
+    public boolean isNumber() { return false;}
+    public boolean isString() { return false;}
+    public int getNumber() {return 0;}
+    public String getString() { return  "";}
 }
